cae50bee4b8d2bee6503f8b9c1bf879acdec1baa
Andre Popovitch <andre@popovit.ch>
2024-05-20 22:37:00 -0500

    Initial Commit

---
+++ DollarDay/DollarDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DollarDayApp.swift
+//  DollarDay
+//
+//  Created by Andre Popovitch on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct DollarDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
